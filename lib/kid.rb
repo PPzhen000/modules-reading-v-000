f41feebbb0eb91ae969d60bccba9d554249259b8
Ariel Zhen <zhenpingping@live.com>
2018-06-07 06:34:00 +0000

    automatically backed up by learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,12 +1,12 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
-require_relative './fancy_dance.rb' 
+require_relative './fancy_dance.rb'
 class Kid
 
   include Dance
 
   extend MetaDancing
-  
+
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
 
